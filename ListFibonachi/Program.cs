246b78ffb5e749aef412aea56eba1dd667bb0f1c
Temito5 <temenugapopova22@icloud.com>
2023-03-01 11:20:40 +0200

    Add project files.

---
+++ ListFibonachi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ulong> numbers=new List<ulong>();
+
+            numbers.Add(1); 
+            numbers.Add(1);
+
+            Console.Write("Koe pored chislo na Fibonachi iskash da ti izchislq?");
+            byte numFib=byte.Parse(Console.ReadLine()); 
+            for (int i = 2; i < numFib; i++)
+            {
+                numbers.Add(numbers[i - 2] + numbers[i-1]);
+            }
+            Console.WriteLine(string.Join(",",numbers));
+        }
+    }
+}
---
+++ PurvaZadacha/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PurvaZadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi broi elementi: ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers= new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Vavedi chislo: ");
+                int item=int.Parse(Console.ReadLine());
+                numbers.Add(item);
+            }
+
+            //int newIndex = n / 2;
+            //numbers.Insert(newIndex, 6);
+
+            //int minElement=numbers.Min();
+            //numbers.Add(minElement);
+
+            while (numbers.Contains(0))
+            {
+                numbers.Remove(0);
+            }
+            
+           
+           
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine($"numbers[{i}]={numbers[i]}");
+            }
+            Console.WriteLine("Broqt na elementite v spicuka e: " + numbers.Count);
+        }
+    }
+}
